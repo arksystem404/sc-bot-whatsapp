0107730e275c402b752e470dd73d5deb911a418a
arxk <148529070+arksystem404@users.noreply.github.com>
2024-07-08 11:56:59 +0700

    Add files via upload

---
+++ V5Media/database/message.js
@@ -0,0 +1,255 @@
+exports.bodymenu = (prefix) => {
+return `_KastrolMods is the WhatsApp Project script_\n_Silahkan melihat fitur WhatsApp_`
+}
+
+exports.dasboard = (prefix) => {
+return `© Script by *KastrolMods*\n• Credit | KastrolMods | 2024`
+}
+
+exports.pambayaran = (prefix) => {
+return `┌─> *PAYMENT* 
+│➮ .dana
+│➮ .gopay
+│➮ .ovo
+└──────────>`
+}
+
+exports.testimoni = (prefix) => {
+return `┌─> *TESTIMONI SHOP* 
+│➮ .testi1 < Testimoni >
+│➮ .testi2 < Testimoni >
+│➮ .testi3 < Testimoni >
+│➮ .testi4 < Testimoni >
+│➮ .testi5 < Testimoni >
+│➮ .testi6 < Testimoni >
+│➮ .testi7 < Testimoni >
+│➮ .testi8 < Testimoni >
+│➮ .testi9 < Testimoni >
+│➮ .testi10 < Testimoni >
+└──────────>`
+}
+
+exports.unbanned = (prefix) => {
+return `┌─> *UNBANNED MENU* 
+│➮ .textunbanv1
+│➮ .textunbanv2
+│➮ .textunbanv3
+│➮ .textunbanv4
+│➮ .textunbanv5
+│➮ .textunbanv6
+│➮ .textunbanv7
+│➮ .textunbanv8
+│➮ .textunbanv9
+│➮ .textunbanv10
+│➮ .textunbanv11
+│➮ .textunbanv12
+│➮ .textunbanv13
+│➮ .textunbanv14
+│➮ .textunbanv15
+│➮ .textunbanv16
+│➮ .textunbanv17
+│➮ .textunbanv18
+│➮ .textunbanv19
+│➮ .textunbanv20
+│➮ .textunbanv21
+└──────────>`
+}
+
+exports.groupstack = (prefix) => {
+return `┌─> *GROUP MENU* 
+│➮ .addmember
+│➮ .hidetag
+│➮ .kick
+│➮ .delete
+│➮ .pppanjanggc
+│➮ .setppgroup
+│➮ .setnamegc
+│➮ .setdesc
+│➮ .cekmember
+│➮ .cekasalmember
+│➮ .getidgrup
+│➮ .getdesk
+│➮ .nfinp
+│➮ .noftication
+│➮ .rules
+└──────────>`
+}
+
+exports.crpanel = (prefix) => {
+return `┌─> *CREATE PANEL* 
+│➮ .1gb < user,nomor >
+│➮ .2gb < user,nomor >
+│➮ .3gb < user,nomor >
+│➮ .4gb < user,nomor >
+│➮ .5gb < user,nomor >
+│➮ .6gb < user,nomor >
+│➮ .7gb < user,nomor >
+│➮ .8gb < user,nomor >
+│➮ .unli < user,nomor >
+└──────────>`
+}
+
+exports.flamingli = (prefix) => {
+return `┌─> *FLAMING MENU* 
+│➮ .flaming1
+│➮ .flaming2
+│➮ .flaming3
+│➮ .flaming4
+│➮ .flaming5
+│➮ .flaming6
+└──────────>`
+}
+
+exports.vidrdm = (prefix) => {
+return `┌─> *RANDOM VIDEO* 
+│➮ .tiktokgirl
+│➮ .tiktoknukhty
+│➮ .tiktoksantuy
+│➮ .tiktokpanrika
+│➮ .tiktoknotnot
+│➮ .tiktokghea
+│➮ .tiktokkayes
+│➮ .tiktokbocil
+└──────────>`
+}
+
+exports.imgrdm = (prefix) => {
+return `┌─> *RANDOM IMAGE* 
+│➮ .tiktokgirl
+│➮ .tiktoknukhty
+│➮ .tiktoksantuy
+│➮ .tiktokpanrika
+│➮ .tiktoknotnot
+│➮ .tiktokghea
+│➮ .tiktokkayes
+│➮ .tiktokbocil
+└──────────>`
+}
+
+exports.ownerlist = (prefix) => {
+return `┌─> *OWNER MENU* 
+│➮ .addcase
+│➮ .delcase
+│➮ .addsaldo
+│➮ .minsaldo
+│➮ .saldonya
+│➮ .saldonya
+│➮ .self/public
+│➮ .addprem
+│➮ .delprem
+│➮ .addlimit
+│➮ .dellimit
+│➮ .upsw
+│➮ .updateinfo
+│➮ .payment
+│➮ .setppbotpanjang
+│➮ .jpm
+│➮ .join
+└──────────>`
+}
+
+exports.qoutedmen = (prefix) => {
+return `┌─> *QUOTES MENU* 
+│➮ .quotesislami
+│➮ .gombal
+│➮ .quotessad
+│➮ .quotesbacot
+│➮ .quotesbucin
+│➮ .quotessindiran
+│➮ .quotespantun
+└──────────>`
+}
+
+exports.primek = (prefix) => {
+return `┌─> *PRIMBO MENU* 
+│➮ .wangy
+│➮ .sangecek
+│➮ .kapankah
+│➮ .cantikcek
+│➮ .jomokcek
+│➮ .gantengcek
+│➮ .rate
+│➮ .bagaimanakah
+│➮ .bisakah
+│➮ .jomokcek
+└──────────>`
+}
+
+exports.inter = (prefix) => {
+return `┌─> *INTERNET MENU* 
+│➮ .capcut
+│➮ .chatgpt
+│➮ .ssweb
+│➮ .sswebv2
+│➮ .prompt-gpt
+│➮ .text2img
+│➮ .pinterest
+└──────────>`
+}
+
+exports.hargapanel = (prefix) => {
+return `*LIST HARGA PANEL RUN BOT 🛒*
+
+  *[ 📦 ] Paket 1* 
+  Ram : 1GB
+  Cpu : 30%
+  Storage : 1GB
+  Price : Rp1.000/Bulan
+
+  *[ 📦 ] Paket 2* 
+  Ram : 2GB
+  Cpu : 40%
+  Storage : 1GB
+  Price : Rp2.000/Bulan
+
+  *[ 📦 ] Paket 3* 
+  Ram : 3GB
+  Cpu : 50%
+  Storage : 2GB
+  Price : Rp3.000/Bulan
+
+  *[ 📦 ] Paket 4* 
+  Ram : 4GB
+  Cpu : 60%
+  Storage : 2GB
+  Price : Rp4.000/Bulan
+
+  *[ 📦 ] Paket 5* 
+  Ram : 5GB
+  Cpu : 70%
+  Storage : 2GB
+  Price : Rp5.000/Bulan
+
+  *[ 📦 ] Paket 6* 
+  Ram : 6GB
+  Cpu : 80%
+  Storage : 3GB
+  Price : Rp6.000/Bulan
+
+  *[ 📦 ] Paket 7* 
+  Ram : 7GB
+  Cpu : 90%
+  Storage : 3GB
+  Price : Rp7.000/Bulan
+
+  *[ 📦 ] Paket 8* 
+  Ram : 8GB
+  Cpu : 100%
+  Storage : 3GB
+  Price : Rp8.000/Bulan
+
+  *[ 📦 ] Paket 9* 
+  Ram : 9GB
+  Cpu : 110%
+  Storage : 4GB
+  Price : Rp9.000/Bulan
+
+  *[ 📦 ] Paket 10* 
+  Ram : Unlimited
+  Cpu : Unlimited
+  Storage : Unlimited
+  Price : Rp10.000/Bulan
+  
+*(Server Private, Garansi 10 Day)*
+Berminat ? Hubungi Owner Bot Dengan Cara Ketik *.owner*`
+}
